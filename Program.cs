5d5ec627ac32e6ac7e84e6303ec73862bece24a2
BlizzCrafter <m.haertel@gmx.net>
2024-10-18 17:13:08 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿using MonoGo.Engine.DesktopGL;
+
+MonoGoPlatform.Init();
+using var game = new MonoGo.Samples.Game1();
+game.Run();
---
+++ SceneFactory.cs
@@ -0,0 +1,33 @@
+﻿using MonoGo.Engine.Drawing;
+using MonoGo.Engine.Resources;
+using MonoGo.Engine.SceneSystem;
+using System;
+
+namespace MonoGo.Samples
+{
+	public class SceneFactory
+	{
+		public Scene Scene;
+		public Type Type { get; private set; }
+
+		public SceneFactory(Type type, string description = "")
+		{
+			Type = type;
+			Description = description;
+		}
+
+		public readonly string Description;
+
+		public void CreateScene()
+		{
+            Text.CurrentFont = ResourceHub.GetResource<IFont>("Fonts", "Default");
+
+            Scene = SceneMgr.CreateScene(Type.Name);
+			Scene.CreateLayer("default");
+			Activator.CreateInstance(Type, Scene["default"]);
+		}
+
+		public void DestroyScene() =>
+			SceneMgr.DestroyScene(Scene);
+	}
+}
